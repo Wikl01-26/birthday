962affa0623d34001af27e3bca44418bb3c9f364
wl <wl01_26@126.com>
2023-03-06 17:16:23 +0800

    提交修改

--- js/diy.js
+++ js/diy.js
@@ -1,8 +1,14 @@
-$(function(){
+$(function () {
     $('#dowebok').fullpage({
         'navigation': true,
-        'navigationPosition':"left",
-        'navigationColor':['#fff'],
+        'navigationPosition': "left",
+        'navigationColor': ['#fff'],
 
     });
+
+    let bir = document.querySelector('.bir');
+    let jm = document.querySelector('.jm');
+    jm.addEventListener('click', function () {
+        bir.autoplay = '';
+    })
 });
